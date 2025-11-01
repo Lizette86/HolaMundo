0a3b5ea38e5a9fb7eb7b2dcc03cb7b4583b2584a
Lizette <lizvan2@alu.edu.gva.es>
2025-11-01 10:41:58 +0100

    Cambio programa 1

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 public class HolaMundo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
         System.out.println("¡Hola Mundo!");
+        System.out.print("¿Cómo te llamas? ");
+        String nombre = sc.nextLine();
+        
+        System.out.println("Encantado de conocerte, " + nombre + "!");
+        
+        sc.close();
     }
 }
 
